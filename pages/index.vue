68c1f50720eed89db674f428953f9b366c7a3f09
Martin Page <wookoouk@gmail.com>
2020-06-16 15:09:00 +0100

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -3,13 +3,14 @@
     <section class="section" v-for="section in history">
       <h2 class="title is-2">{{section.name}}</h2>
       <ul v-for="pkg in section.packages">
-        
-        <li>{{pkg.name}} - {{pkg.path}}</li>
-<!-- 
+        <li>
+          <a>{{pkg.name}} - {{pkg.path}}</a>
+        </li>
+        <!-- 
 
         <h3 class="title is-3"></h3>
         <h4 class="title is-4">Versions</h4>
-        <p v-for="version in pkg.versions">{{version}}</p> -->
+        <p v-for="version in pkg.versions">{{version}}</p>-->
       </ul>
     </section>
   </div>
